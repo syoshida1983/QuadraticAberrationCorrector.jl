5444e076bf9088e36eed7a9206ead1d3898d5013
Documenter.jl <documenter@juliadocs.github.io>
2023-04-26 15:07:27 +0000

    build based on 95870a5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuadraticAberrationCorrector","category":"page"},{"location":"#QuadraticAberrationCorrector","page":"Home","title":"QuadraticAberrationCorrector","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuadraticAberrationCorrector.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuadraticAberrationCorrector]","category":"page"}]
+}
